7d1350de01148274c1bed8a3dff1ffffd27d6267
casey <casey@10.0.0.3>
2018-03-04 10:17:45 -0600

    Adding the initial project and Hello.java file to the repository.

---
+++ src/main/Hello.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * Prints a message out to the console.
+ * 
+ * @author casey
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+
+}
